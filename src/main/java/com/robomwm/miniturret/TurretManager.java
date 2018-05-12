06f4b3d1d26c5ae8774440823488e7987555fe17
RoboMWM <robomwm@users.noreply.github.com>
2018-05-12 02:06:19 -0700

    try this for ez offset

--- src/main/java/com/robomwm/miniturret/TurretManager.java
+++ src/main/java/com/robomwm/miniturret/TurretManager.java
@@ -103,7 +103,7 @@ public class TurretManager implements Listener
                                     return;
                                 }
                                 Vector vector = entity.getEyeLocation().toVector().subtract(turret.getEyeLocation().toVector());
-                                Arrow arrow = turret.getWorld().spawnArrow(turret.getEyeLocation().add(0, 0.5, 0), vector, 2, 0);
+                                Arrow arrow = turret.getWorld().spawnArrow(turret.getEyeLocation().add(vector.clone().normalize()), vector, 2, 0);
                                 arrow.setKnockbackStrength(100);
                                 arrow.spigot().setDamage(2D);
                                 turret.teleport(turret.getLocation().setDirection(vector));
